22c2cc7ec4fc23a3e76b5114225de051f0c8bed8
Michaela Gasteratou <elagasteratou@gmail.com>
2021-03-01 21:58:36 +0000

    edited the game to ask if you want to play again

--- rps_game.py
+++ rps_game.py
@@ -1,6 +1,26 @@
 import random
 
 # funtions
+
+def ask_players_input():
+    # variables
+    random_number = random.randint(0, 2)
+    comp_choices = ["Rock", "Paper", "Scissors"]
+    computer = comp_choices[random_number]
+    game_on = False
+
+    # logic
+    while game_on is False:
+        player1_input = input("Please type R for Rock, P for Paper or S for Scissors: >> \n").upper()
+        if (player1_input == "R") or (player1_input == "P") or (player1_input == "S"):
+            game_on = True
+            player_choice = convert_player_input(player1_input)
+            # Added game results
+            game_result = start_game(player_choice, computer)
+            print(game_result)
+            # start the game again?
+            play_again()
+
 def convert_player_input(letter):
     if letter == "R":
         return "Rock"
@@ -9,43 +29,30 @@ def convert_player_input(letter):
     else:
         return "Scissors"
 
-
-def playing(player_choice, computer):
+def start_game(player_choice, computer):
     if player_choice == computer:
-        return "It's a draw! Computer input was " + computer + " and your input was " + player_choice
+        return f"It's a draw! \nComputer input was {computer} \nand your input was {player_choice}"
     elif (player_choice == "Rock" and computer == "Scissors") or (player_choice == "Paper" and computer == "Rock") or (player_choice == "Scissors" and computer == "Paper"):
-        return "You Win!!! Computer input was " + computer + " and your input was " + player_choice
+        return f"You Win!!! \nComputer input was {computer} \nand your input was {player_choice}"
     else:
-        return "Computer Wins!!! Computer input was " + computer + " and your input was " + player_choice
+        return f"Computer Wins!!! \nComputer input was {computer} \nand your input was {player_choice}"
+
+def play_again():
+    another_game = input("Shall we play again? 'Y' or 'N'\n").upper()
+    if another_game == "Y":
+        ask_players_input()
+    elif another_game == "N":
+        print("Have a great day!\t Play again soon")
+    else:
+        play_again()
 
 
-# variables
-random_number = random.randint(0, 2)
-comp_choices = ["Rock", "Paper", "Scissors"]
-computer = comp_choices[random_number]
-# print(computer)
-game_on = False
+ask_players_input()
 
-# logic
-while game_on is False:
-    player1_input = input("Please type R for Rock, P for Paper or S for Scissors: >> \n").upper()
-    if (player1_input == "R") or (player1_input == "P") or (player1_input == "S"):
-        game_on = True
-        player_choice = convert_player_input(player1_input)
-        # Added game results
-        game_result = playing(player_choice, computer)
-        print(game_result)
+
+
     # else:
     #     player1_input = input("Please type R for Rock, P for Paper or S for Scissors: >> \n").upper()
-
-
-
-
-
-
-
-
-
 
 
     # while player1_input != "R" or "P" or "S":
